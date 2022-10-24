f1d78c7d4cf0744bd8469cc14b4cd35659271ded
PanInf <77333045+PanInf@users.noreply.github.com>
2022-10-24 10:45:00 +0000

    KartaPracy3 - wykonanie Zad 3-4

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -43,4 +43,48 @@
 
 # Zad 3 
 
+# n = int(input())
+# for i in range(1, n+1):
+#     if n % i == 0:
+#         print(i, end=" ")
+
+# Zad 4
+
+# s = 0
+# for i in range(10,100):
+#     s = s + i
+# print(s)
+
+# Zad 5
+
+n = int(input())
+
+for i in range(n-1):
+    k = int(input())
+    
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
