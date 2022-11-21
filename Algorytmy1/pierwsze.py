ccfb9045aef80eb907aa39853a55943fc6ce1284
PanInf <77333045+PanInf@users.noreply.github.com>
2022-11-21 11:54:00 +0000

    generowanie liczb pierwszych

--- Algorytmy1/pierwsze.py
+++ Algorytmy1/pierwsze.py
@@ -3,12 +3,23 @@
 # 2,3,5,7,11,13,17,19,23 .. itd
 # dzielniki właściwe - dzielniki liczby poza 1 i nią samą
 
-n = int(input())
-for i in range(2, n):
-    if n % i == 0:
-        print("Liczba nie jest pierwsza")
-        exit(0)
-print("Liczba jest pierwsza")
+# n = int(input())
+# for i in range(2, n):
+#     if n % i == 0:
+#         print("Liczba nie jest pierwsza")
+#         exit(0)
+# print("Liczba jest pierwsza")
 
 # 2. Generowanie liczb pierwszych
 
+
+n = int(input("Podaj do ilu mam szukać liczb pierwszych: \n"))
+
+for i in range(2, n+1):
+    flaga = True
+    for j in range(2,int(i**0.5)+1):
+        if i % j == 0:
+            flaga = False
+            break
+    if flaga == True:
+        print(i, end=" ")
